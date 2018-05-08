0a47680d2b32b771c975b32769e58e0df748a170
Scott Egbert <scottregb@gmail.com>
2018-05-08 11:27:30 -0500

    Users can see survey results

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,10 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+// use to test a manual send in console using with const survey = { title: 'my title', subject: 'my subject', body: 'heres the body of the email', recipients: 'putyouremailaddresshere' }; then do this axios.post('/api/surveys', survey);
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
